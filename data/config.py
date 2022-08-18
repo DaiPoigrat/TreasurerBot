5932f5d5f3734dc365389ab790448869d6d6f187
DaiPoigrat <kuluanov@gmail.com>
2022-08-18 22:30:00 +0400

    .env

--- data/config.py
+++ data/config.py
@@ -8,4 +8,3 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа s
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 DB_URI = env.str("URI")
-уб
