865de3fa45fd5afe6c45c3a2f6878e5e25eea08b
DaiPoigrat <kuluanov@gmail.com>
2022-08-18 22:00:44 +0400

    Начал переписывать под базу данных

--- data/config.py
+++ data/config.py
@@ -6,7 +6,5 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-CFO = []  # Тут у нас будет список из cfo
-TUTOR = []  # Тут у нас будет список из юристов
-ACCOUNTANT = []  # Тут у нас будет список из бухгалтеров
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+DB_URI = env.str("URI")
