d5e456a0a9898e62230af542d3028ae80c605eaf
DaiPoigrat <kuluanov@gmail.com>
2022-08-18 22:38:53 +0400

    Настройка окружения под heroku

--- data/config.py
+++ data/config.py
@@ -7,4 +7,4 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
-DB_URI = env.str("URI")
+DB_URI = env.str("DATABASE_URL")
