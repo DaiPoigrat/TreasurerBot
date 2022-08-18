7f658e4841cfbb3351e529e62662bfb9af86deb9
DaiPoigrat <kuluanov@gmail.com>
2022-08-18 22:12:41 +0400

    Убрал мусор из пакета users

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -3,7 +3,4 @@ from . import start
 # from . import echo
 from . import payment_iniciator
 from . import admin
-from . import cfo
-from . import tutor
-from . import accountant
 from . import general_handlers
