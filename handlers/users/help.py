1e8df12af2bad858a21fd7548943d1948776cd79
DaiPoigrat <kuluanov@gmail.com>
2022-08-18 22:06:51 +0400

    Убрал мусор из help

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -1,7 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
 
-from data.config import ADMINS, CFO, TUTOR, ACCOUNTANT
+from data.config import ADMINS
 from loader import dp
 
 
@@ -11,27 +11,9 @@ async def bot_help(message: types.Message):
     if str(user_id) in ADMINS:
         text = ("Список команд: ",
                 "/dbmanage - управление базой данных")
-    elif user_id in CFO:
-        text = ("Список команд: ",
-                "/toexcel - выгрузить реестр в excel",
-                "/agree - согласовать плтежи",
-                "/question - связаться с инициатором платежа",
-                "/block - заблокировать платеж",
-                "/final - выгрузить итоговыый реестр")
-    elif user_id in TUTOR:
-        text = ("Список команд: ",
-                "/agree - согласовать плтежи",
-                "/question - связаться с инициатором платежа",
-                "/block - заблокировать платеж")
-    elif user_id in ACCOUNTANT:
-        text = ("Список команд: ",
-                "/agree - согласовать плтежи",
-                "/question - связаться с инициатором платежа",
-                "/block - заблокировать платеж")
     else:
         text = ("Список команд: ",
                 "/start - Начать диалог",
-                "/help - Получить справку",
-                "/payment - заполнить заявку")
+                "/help - Получить справку")
 
     await message.answer("\n".join(text))
