f3fa9711563d88cdd517459d234259025edf4bce
DaiPoigrat <kuluanov@gmail.com>
2022-08-23 15:36:55 +0400

    Отправка юзеру списка его операций

--- utils/db_api/create_registry.py
+++ utils/db_api/create_registry.py
@@ -95,6 +95,6 @@ def get_records_by_name(user_name: str) -> None:
 
     line = []
     for row in range(2, register_sheet.max_row + 1):
-        logging.info(msg=f'{register_sheet.cell(row=row, column=2)}')
+        logging.info(msg=f'{register_sheet.cell(row=row, column=2).value}')
         if register_sheet.cell(row=row, column=2) == user_name:
             logging.info(msg=f'НАШЛИ ЗАПИСЬ')
