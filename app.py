af066cb48304e6a66d88a622f1e124a0a55feb9f
DaiPoigrat <kuluanov@gmail.com>
2022-08-27 14:30:47 +0400

    Оповещение

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ async def on_startup(dispatcher):
 
     # Уведомляет про запуск
     await on_startup_notify(dispatcher)
+    await on_shutdown_notify(dispatcher)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown_notify)
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -7,9 +7,17 @@ from data.config import ADMINS
 
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
-        logging.info(msg=f'ADMIN = {admin}')
         try:
             await dp.bot.send_message(admin, "Бот Запущен")
+
+        except Exception as err:
+            logging.exception(err)
+
+
+async def on_shutdown_notify(dp: Dispatcher):
+    for admin in ADMINS:
+        try:
+            await dp.bot.send_message(admin, "Бот Закрыт")
 
         except Exception as err:
             logging.exception(err)
