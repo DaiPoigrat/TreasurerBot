9b835505f843ae757ec87b1e99bd8d7a20353233
DaiPoigrat <kuluanov@gmail.com>
2022-08-27 20:13:39 +0400

    фикс

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -29,11 +29,11 @@ async def startChatting(call: CallbackQuery, state: FSMContext):
     user_id = int(data[2])
     user_state = dp.current_state(chat=user_id, user=user_id)
 
-    logging.info(msg=f'USER STATE == {user_state.get_state()}')
+    logging.info(msg=f'USER STATE == {await user_state.get_state()}')
 
     await user_state.set_state(state=Chatting.ToAdmin)
 
-    logging.info(msg=f'USER STATE == {user_state.get_state()}')
+    logging.info(msg=f'USER STATE == {await user_state.get_state()}')
 
     username = data[3]
     await state.update_data(
