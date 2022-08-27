7cec86695c8c6d6192bd3e9774b13a10f6b486a2
DaiPoigrat <kuluanov@gmail.com>
2022-08-27 13:59:04 +0400

    проверка оповещения

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -7,6 +7,7 @@ from data.config import ADMINS
 
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
+        logging.info(msg=f'ADMIN = {admin}')
         try:
             await dp.bot.send_message(admin, "Бот Запущен")
 
